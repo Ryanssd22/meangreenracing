30a0a21f07adaa82b91bc45a1238c2f59f3d30ac
Ryanssd22 <47930762+Ryanssd22@users.noreply.github.com>
2025-09-02 10:19:35 -0700

    Enhanced Images test

--- src/lib/components/HeroImage.svelte
+++ src/lib/components/HeroImage.svelte
@@ -6,13 +6,16 @@
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
 	import { quintIn, quintOut } from 'svelte/easing';
-	const heroImages = import.meta.glob('/src/lib/assets/sae_photos/hero/*.jpg');
+	const heroImages = import.meta.glob('/src/lib/assets/sae_photos/hero/*.jpg', {
+		query: { enhanced: true }
+	});
 
-	let currentImage = $state(undefined);
-	let preloadedImage = $state(undefined);
+	// let currentImage = $state('$lib/assets/sae_photos/hero/9F0A3610.jpg?enhanced');
+	let currentImage = $state(null);
+	let preloadedImage = $state(null);
 	let heroImagesValues = Object.values(heroImages);
 	onMount(async () => {
-		const IMAGE_INTERVAL = 7000;
+		const IMAGE_INTERVAL = 4000;
 
 		await chooseRandomImage();
 		let randomImageInterval = setInterval(async () => {
@@ -27,6 +30,7 @@
 		// Setting current image
 		if (!preloadedImage) {
 			const index = Math.floor(Math.random() * heroImagesValues.length);
+      console.log("Getting", index)
 			let imageModule = await heroImagesValues[index]();
 			currentImage = imageModule.default;
 		} else {
@@ -40,16 +44,22 @@
 
 		console.log('Image switched to', index);
 	}
+
+  // $inspect(currentImage);
 </script>
 
 <div class="relative h-screen w-full overflow-hidden">
-	{#key currentImage}
-		<img
-			alt="Hero"
-			src={currentImage}
-			in:fly={{ y: 25, duration: 2000, easing: quintOut }}
-			out:fly={{ duration: 4000, easing: quintIn }}
-			class="absolute inset-0 h-full w-full object-cover"
-		/>
-	{/key}
+	{#if currentImage}
+		{#key currentImage}
+			<enhanced:img
+				alt="Hero"
+				src={currentImage}
+				in:fly={{ y: 25, duration: 2000, easing: quintOut }}
+				out:fly={{ duration: 4000, easing: quintIn }}
+				class="absolute inset-0 h-full w-full object-cover"
+			/>
+		{/key}
+	{:else}
+		<p>Loading...</p>
+	{/if}
 </div>
