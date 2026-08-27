bf4e99bbf7932db519be2b2a9f2c20355c979a69
Benedict Simeon <carbox7@gmail.com>
2026-08-26 22:23:39 -0500

    Added some light transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
 
 	// Hero Image
 	import { PUBLIC_CLOUDFLARE_S2_ENDPOINT } from '$env/static/public';
-	let heroImagesValues = $state(["https://4fzz3fozay0zmmgv.public.blob.vercel-storage.com/hero/DSC00589%20%281%29.webp"])
+	let heroImagesValues = $state(["https://ryansserver.org/gallery/2015/misc/20140919-DSC00945.webp"])
 	data.hero_response.then((response) => {
 		console.log("HERO RESPONSE:", response);
 		heroImagesValues = response?.Contents.map(content => (PUBLIC_CLOUDFLARE_S2_ENDPOINT + content.Key));
--- src/routes/media/[folder]/+page.svelte
+++ src/routes/media/[folder]/+page.svelte
@@ -4,49 +4,48 @@
     import '$lib/photoswipe/photoswipe.css';
     import PhotoSwipeLightbox from '$lib/photoswipe/photoswipe-lightbox.esm.js';
     import { PUBLIC_CLOUDFLARE_S2_ENDPOINT } from '$env/static/public';
+    import { page } from '$app/state';
+    import { fly } from 'svelte/transition';
 
     const { data } = $props();
-    
-    let images = $state([]);
-    let lightbox;
 
-    // Resolve the promise and store image data
+    // GETTING CLOUDFLARE IMAGES
+    let images = $state([]);
     data.gallery_response.then((response) => {
         console.log("RESPONSE:", response)
         images = response?.Contents.map(image => (PUBLIC_CLOUDFLARE_S2_ENDPOINT + image.Key));
+        images.shift();
         console.log("IMAGES:", images);
     });
+    let test = $state([1,2,3,4])
 
-    // Initialize Lightbox once images are rendered in the DOM
-    $effect(() => {
-        if (images.length > 0) {
-            lightbox = new PhotoSwipeLightbox({
-                gallery: '#my-gallery',
-                children: 'a',
-                pswpModule: () => import('$lib/photoswipe/photoswipe.esm.js')
-            });
-            lightbox.init();
-
-            // Cleanup when component unmounts or images update
-            return () => {
-                lightbox.destroy();
-            };
-        }
-    });
+    // PHOTOSWIPE SETUP
+    onMount(() => {
+        const lightbox = new PhotoSwipeLightbox({
+            gallery: '#gallery--main',
+            children: 'a',
+            pswpModule: () => import('$lib/photoswipe/photoswipe.esm.js')
+        });
+        lightbox.init();
+    })
 </script>
 
-<div class="pswp-gallery" id="my-gallery">
-    <div class="grid grid-cols-5 gap-2">
-    {#each images as image}
-        <!-- <a 
-            href={image.url} 
-            data-pswp-width={image.width} 
-            data-pswp-height={image.height} 
-            target="_blank"
-            rel="noreferrer"
-        > -->
-            <img src={image} alt="" class="w-25 h-25" />
-        <!-- </a> -->
-    {/each}
-    </div>
+<div class="flex flex-col items-center min-h-screen">
+    <h2 class="font-bold text-2xl">MEDIA TEST</h2>
+    {#if images.length != 0}
+        <div class="pswp-gallery grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-4" id="gallery--main">
+        {#each images.entries() as [i, image]}
+            <a in:fly|global={{delay: i*10}} class="block w-40 h-40 bg-blue-500 overflow-hidden" 
+                data-pswp-srcset={image} 
+                target="_blank" href={image}
+                data-pswp-height="1080" data-pswp-width="1920"
+                rel="noreferrer">
+                <p class="text-[8px]">{image.slice(PUBLIC_CLOUDFLARE_S2_ENDPOINT.length + page.params?.folder.length + 8)}</p>
+                 <!-- <img src={image} alt="" class="w-40 h-50 object-cover"/> -->
+            </a>
+        {/each}
+        </div>
+    {:else}
+        <h2>Loading...</h2>
+    {/if}
 </div>
