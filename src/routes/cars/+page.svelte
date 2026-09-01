3b2560db11277426a5934f57bd58adc0337dbe8e
Ryanssd22 <carbox7@gmail.com>
2026-08-31 23:18:25 -0500

    Fixed cars

--- src/routes/cars/+page.svelte
+++ src/routes/cars/+page.svelte
@@ -1,25 +1,26 @@
 <!--CARS-->
 <script>
     // Gaussian Splats
-    const splats = [
+    let splats = $state([
         {
             "title": "F26",
-            "src": "/splat/seacar.html"
+            "src": "/splat/saecar.html"
         },
         {
             "title": "F17",
             "src": "/splat/f17.html"
         },
-    ]
+    ])
 </script>
 
-<div>
+<div class="px-2">
     {#each splats as splat}
-        <iframe title={splat.title} src={splat.src} class="w-[70em] h-[50em]"></iframe>
+        <h2 class="py-5 text-4xl font-bold">{splat.title}</h2>
+        <iframe title={splat.title} src={splat.src} class="w-150 h-120"></iframe>
     {/each}
     <!-- <iframe title="car" src="/splat/saecar.html" class="w-[70em] h-[50em]">
-    </iframe>
+    </iframe> -->
 
-    <iframe title="f17" src="/splat/f17.html" class="w-[70em] h-[50em]">
+    <!-- <iframe title="f17" src="/splat/f17.html" class="w-[70em] h-[50em]">
     </iframe> -->
 </div>
